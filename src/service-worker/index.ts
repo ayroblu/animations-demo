7069e57eaa3763272c15afab582945a09e054fb6
Ben Lu <ayroblu@gmail.com>
2023-12-23 00:57:42 +0000

    add sw

---
+++ src/service-worker/index.ts
@@ -0,0 +1,29 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+declare const self: ServiceWorkerGlobalScope;
+
+import {
+  cleanupStaleAssets,
+  handleNavigationPreload,
+  handlePrecache,
+  proxyFetch,
+} from "./main";
+
+self.addEventListener("fetch", proxyFetch);
+
+self.addEventListener("install", handlePrecache);
+// Skip Waiting makes the new service worker the active service worker
+self.addEventListener("install", () => self.skipWaiting());
+// claim makes the new requests go to the new service worker
+self.addEventListener("activate", (event) =>
+  event.waitUntil(self.clients.claim()),
+);
+self.addEventListener("activate", cleanupStaleAssets);
+
+/** Since our app is prerendered, we don't need navigation preload */
+const withNavigationPreload = false;
+if (withNavigationPreload) {
+  self.addEventListener("activate", handleNavigationPreload);
+}
--- src/service-worker/main.ts
+++ src/service-worker/main.ts
@@ -1,101 +1,120 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
 /// <reference lib="webworker" />
+import { build, files, prerendered } from "$service-worker";
 declare const self: ServiceWorkerGlobalScope;
-import type { PrecacheEntry } from './types';
 
-const cacheName = `sw-precache-${self.registration.scope}`;
-const runtimeCacheName = `sw-runtimecache-${self.registration.scope}`;
+// if you ever change this, you need to delete existing cache storage
+const version = "1";
+const cacheName = `sw-precache-v${version}`;
 const precacheRoutes = new Set();
-const mappedRoutes = {
-	[self.registration.scope]: `${self.registration.scope}index.html`
-};
-const runtimePaths = [`${self.registration.scope}favicons`, `${self.registration.scope}assets`];
-const workboxManifest = self.__WB_MANIFEST || [];
-// console.log("manifest", workboxManifest);
+const cacheablePaths = [
+  ...build, // the js and css and related parts of the app itself
+  ...files, // everything in `static`
+  ...prerendered, // routes marked as prerendered
+];
+const prerenderedSet = new Set(prerendered);
 
 // MARK: install
-export function precacheManifest(event: ExtendableEvent) {
-	event.waitUntil(handlePrecacheManifest());
+export function handlePrecache(event: ExtendableEvent) {
+  event.waitUntil(handlePrecacheManifest());
 }
 async function handlePrecacheManifest() {
-	const cacheablePaths = getCacheablePaths(workboxManifest);
-	saveRoutes(cacheablePaths);
-	const cache = await caches.open(cacheName);
-	const cachedRequests = await cache.keys();
-	const cachedUrls = new Set(cachedRequests.map(({ url }) => url));
-	const newPaths = cacheablePaths.filter((url) => !cachedUrls.has(url));
+  saveRoutes(cacheablePaths);
+  const cache = await caches.open(cacheName);
+  const cachedRequests = await cache.keys();
+  const cachedUrls = new Set(
+    cachedRequests
+      .map(({ url }) => url)
+      .map((url) => url.replace(self.location.origin, "")),
+  );
+  // NOTE: prerendered routes must always be fetched
+  const newPaths = cacheablePaths.filter(
+    (url) => !cachedUrls.has(url) || prerenderedSet.has(url),
+  );
+  log("sw: new precache:", newPaths.length, "/", cacheablePaths.length);
 
-	for (const path of newPaths) {
-		// console.log("new precache path", path);
-		const reqUrl = new URL(path);
-		await cache.add(reqUrl);
-	}
+  for (const path of newPaths) {
+    const reqUrl = new URL(path, self.location.origin);
+    // TODO: update tests to support URL param to fetch
+    await cache.add(reqUrl);
+  }
 }
 function saveRoutes(routes: string[]) {
-	for (const route of routes) {
-		const url = new URL(route);
-		const simplifiedRoute = `${url.origin}${url.pathname}`;
-		precacheRoutes.add(simplifiedRoute);
-	}
+  for (const route of routes) {
+    const url = new URL(route, self.location.origin);
+    const simplifiedRoute = `${url.origin}${url.pathname}`;
+    precacheRoutes.add(simplifiedRoute);
+  }
 }
 
 // MARK: activate
 export function cleanupStaleAssets(event: ExtendableEvent) {
-	event.waitUntil(handleRemoveStaleAssets());
+  event.waitUntil(handleRemoveStaleAssets());
 }
 async function handleRemoveStaleAssets() {
-	const cacheablePathsSet = new Set(getCacheablePaths(workboxManifest));
-	const cache = await caches.open(cacheName);
-	const cachedRequests = await cache.keys();
-	const staleRequests = cachedRequests.filter((req) => !cacheablePathsSet.has(req.url));
-	await Promise.all(staleRequests.map((req) => cache.delete(req)));
+  const cacheablePathsSet = new Set(cacheablePaths);
+  const cache = await caches.open(cacheName);
+  const cachedRequests = await cache.keys();
+  const staleRequests = cachedRequests.filter(
+    (req) => !cacheablePathsSet.has(req.url.replace(self.location.origin, "")),
+  );
+  log(
+    "removing stale assets:",
+    staleRequests.length,
+    "/",
+    cachedRequests.length,
+  );
+  await Promise.all(staleRequests.map((req) => cache.delete(req)));
+}
+
+export function handleNavigationPreload(event: ExtendableEvent) {
+  event.waitUntil(setNavigationPreload());
+}
+async function setNavigationPreload() {
+  if (self.registration.navigationPreload) {
+    await self.registration.navigationPreload.enable();
+    // How to use
+    // const response = await event.preloadResponse;
+    // if (response) return response;
+  }
 }
 
 // MARK: fetch
 export function proxyFetch(event: FetchEvent) {
-	const url = new URL(event.request.url);
-	const route = `${url.origin}${url.pathname}`;
-	if (precacheRoutes.has(route)) {
-		event.respondWith(handlePrefetch(event));
-	} else if (mappedRoutes[route]) {
-		event.respondWith(handlePrefetch(event, mappedRoutes[route]));
-	} else if (runtimePaths.some((path) => route.startsWith(path))) {
-		event.respondWith(handleRuntimeFetch(event));
-	} else {
-		// console.log("unhandled route", route);
-	}
+  const url = new URL(event.request.url);
+  const route = `${url.origin}${url.pathname}`;
+  if (precacheRoutes.has(route)) {
+    event.respondWith(handlePrefetch(event));
+  } else {
+    log("unhandled route", route);
+    unhandledRoutesLog.push(route);
+  }
 }
-async function handlePrefetch(event: FetchEvent, url: string = event.request.url) {
-	const cache = await caches.open(cacheName);
-	const match = await cache.match(url, { ignoreSearch: true });
-	if (match) {
-		return match;
-	}
-	const res = await fetch(event.request.clone());
-	return res;
-}
-async function handleRuntimeFetch(event: FetchEvent) {
-	const cache = await caches.open(runtimeCacheName);
-	// CacheFirst
-	const match = await cache.match(event.request, { ignoreSearch: true });
-	if (match) {
-		return match;
-	}
-	const response = await fetch(event.request);
-	if (response.ok) {
-		cache.put(event.request, response.clone());
-	}
-	return response;
+const unhandledRoutesLog: string[] = [];
+async function handlePrefetch(
+  event: FetchEvent,
+  url: string = event.request.url,
+) {
+  const cache = await caches.open(cacheName);
+  const match = await cache.match(url, { ignoreSearch: true });
+  if (debug && unhandledRoutesLog.length) {
+    log("missing:", unhandledRoutesLog);
+  }
+  if (match) {
+    return match;
+  }
+  const res = await fetch(event.request);
+  if (res.ok) {
+    await cache.put(url, res.clone());
+  }
+  return res;
 }
 
-// MARK: utils
-function getCacheablePaths(workboxManifest: (PrecacheEntry | string)[]): string[] {
-	const manifestEntries = workboxManifest as PrecacheEntry[];
-	const cacheableUrls = manifestEntries.map(({ revision, url }) => {
-		const reqUrl = new URL(self.registration.scope + url);
-		if (revision) {
-			reqUrl.searchParams.append('__WB_REVISION__', revision);
-		}
-		return reqUrl.href;
-	});
-	return cacheableUrls;
+const debug = true;
+function log(...args: unknown[]) {
+  if (debug) {
+    console.log(...args);
+  }
 }
